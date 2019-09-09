7f3620d39204268a28f14221c6ce59b3dcdb2520
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2019-09-09 17:51:09 -0300

    Carregando classes automaticamente

--- 05-arrays/index.php
+++ 05-arrays/index.php
@@ -1,6 +1,8 @@
 <?php
 
-require 'Calculadora.php';
+namespace Alura;
+
+require 'autoload.php';
 
 $notas = [8, 8, 9, 10];
 
@@ -13,3 +15,24 @@ if($media) {
 } else {
     echo "Não foi possível calcular a média";
 }
+
+echo "=========" . PHP_EOL;
+
+$correntistas_e_compras = [
+    "Giovanni",
+     12,
+    "Maria",
+     25,
+    "Luis",
+    "Luísa",
+    "12"
+];
+
+echo "<pre>";
+var_dump($correntistas_e_compras);
+
+ArrayUtils::remover("12", $correntistas_e_compras);
+
+var_dump($correntistas_e_compras);
+
+echo "</pre>";
---
+++ 05-arrays/src/Alura/Calculadora.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Alura;
+
+class Calculadora
+{
+    public function calculaMedia(array $notas) : ?float
+    {
+        $quantidadeNotas = sizeof($notas);
+
+        $soma = 0;
+
+        if($quantidadeNotas > 0) {
+            for ($i = 0; $i < $quantidadeNotas; $i++) {
+                $soma += $notas[$i];
+            }
+
+            $media = $soma / $quantidadeNotas;
+
+            return $media;
+        } else {
+            return false;
+        }
+    }
+}
