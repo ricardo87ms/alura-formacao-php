0afabde1006ba82db399e210fcd2f101c4efc607
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2020-02-04 19:04:17 -0300

    Integração com outros componentes

--- 12-doctrine-orm/src/Helpers/EntityManagerFactory.php
+++ 12-doctrine-orm/src/Helpers/EntityManagerFactory.php
@@ -15,17 +15,17 @@ class EntityManagerFactory
             [$rootDir . '/src'],
             true
         );
-        $connection = [
-            'driver' => 'pdo_sqlite',
-            'path' => $rootDir . '/var/data/banco.sqlite'
-        ];
         // $connection = [
-        //     'driver' => 'pdo_mysql',
-        //     'host' => 'localhost',
-        //     'dbname' => 'curso_doctrine',
-        //     'user' => 'root',
-        //     'password' => 'root'
+        //     'driver' => 'pdo_sqlite',
+        //     'path' => $rootDir . '/var/data/banco.sqlite'
         // ];
+        $connection = [
+            'driver' => 'pdo_mysql',
+            'host' => 'localhost',
+            'dbname' => 'curso_doctrine',
+            'user' => 'root',
+            'password' => 'root'
+        ];
         return EntityManager::create($connection, $config);
     }
 }
