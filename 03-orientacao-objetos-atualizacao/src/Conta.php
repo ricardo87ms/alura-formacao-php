b1b341adde406fd25550bb935b8774fa8ee1a504
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2020-09-22 19:07:57 -0300

    Definindo Comportamentos

--- 03-orientacao-objetos-atualizacao/src/Conta.php
+++ 03-orientacao-objetos-atualizacao/src/Conta.php
@@ -4,5 +4,34 @@ class Conta
 {
     public $cpfTitutar;
     public $nomeTitular;
-    public $saldo;
+    public $saldo = 0;
+
+    public function sacar(float $valorASacar): void
+    {
+        if ($valorASacar > $this->saldo) {
+            echo 'Saldo Indisponível!!!';
+            return;
+        }
+        $this->saldo -= $valorASacar;
+    }
+
+    public function depositar(float $valorADepositar): void
+    {
+        if ($valorADepositar < 0) {
+            echo 'Não é possível depositar um valor negativo!!!';
+            return;
+        }
+        $this->saldo += $valorADepositar;
+    }
+
+    public function transferir(float $valorATransferir, Conta $contaDestino): void
+    {
+        if ($valorATransferir > $this->saldo) {
+            echo 'Não existe saldo disponível para transferência!!!';
+            return;
+        }
+
+        $this->sacar($valorATransferir);
+        $contaDestino->depositar($valorATransferir);
+    }
 }
