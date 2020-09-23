433a97485ec3fe767cc09740285b6a58eae6a515
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2020-09-23 19:10:36 -0300

    03 atualização - Composição de Objetos

--- 03-orientacao-objetos-atualizacao/banco.php
+++ 03-orientacao-objetos-atualizacao/banco.php
@@ -1,12 +1,18 @@
 <?php
 
 require_once 'src/Conta.php';
+require_once 'src/Titular.php';
+require_once 'src/Cpf.php';
 
 // $primeiraConta = new Conta('999.999.444-22', 'Ana'); //Erro validação
 
-$primeiraConta = new Conta('999.444.444-22', 'Ricardo Moreira Soares');
+$ricardo = new Titular(new Cpf('999.444.444-22'), 'Ricardo Moreira Soares');
 
-$segundaConta = new Conta('999.343.444-22', 'Teste de Nome');
+$primeiraConta = new Conta($ricardo);
+
+$teste = new Titular(new Cpf('999.343.444-22'), 'Teste de Nome');
+
+$segundaConta = new Conta($teste);
 
 // $primeiraConta->defineCpfTitular('999.999.444-22');
 
--- 03-orientacao-objetos-atualizacao/src/Conta.php
+++ 03-orientacao-objetos-atualizacao/src/Conta.php
@@ -2,16 +2,13 @@
 
 class Conta
 {
-    public $cpfTitutar;
-    public $nomeTitular;
+    private $titular;
     private $saldo;
     private static $numeroDeContas = 0;
 
-    public function __construct(string $cpfTitular, string $nomeTitular)
+    public function __construct(Titular $titular)
     {
-        $this->cpfTitutar = $cpfTitular;
-        $this->validaNomeTitular($nomeTitular);
-        $this->nomeTitular = $nomeTitular;
+        $this->titular = $titular;
         $this->saldo = 0;
 
         self::$numeroDeContas++;
@@ -63,7 +60,7 @@ class Conta
 
     public function recuperaCpfTitular()
     {
-        return $this->cpfTitutar;
+        return $this->titular->recuperaCpf();
     }
 
     // public function defineNomeTitular(string $nome)
@@ -73,15 +70,7 @@ class Conta
 
     public function recuperaNomeTitular()
     {
-        return $this->nomeTitular;
-    }
-
-    private function validaNomeTitular(string $nomeTitular)
-    {
-        if (strlen($nomeTitular) < 5) {
-            echo "Nome precisa ter pelo menos 5 caracteres";
-            exit();
-        }
+        return $this->titular->recuperaNome();
     }
 
     public static function recuperaNumeroDeContas()
