af553416b2c69508ddfcdd1ce99fabc67dbc58ed
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2020-09-23 18:32:27 -0300

    03 atualizacao - Mais sobre métodos

--- 03-orientacao-objetos-atualizacao/banco.php
+++ 03-orientacao-objetos-atualizacao/banco.php
@@ -2,11 +2,15 @@
 
 require_once 'src/Conta.php';
 
-$primeiraConta = new Conta();
+// $primeiraConta = new Conta('999.999.444-22', 'Ana'); //Erro validação
 
-$primeiraConta->defineCpfTitular('999.999.444-22');
+$primeiraConta = new Conta('999.444.444-22', 'Ricardo Moreira Soares');
 
-$primeiraConta->defineNomeTitular('Ricardo Moreira Soares');
+$segundaConta = new Conta('999.343.444-22', 'Teste de Nome');
+
+// $primeiraConta->defineCpfTitular('999.999.444-22');
+
+// $primeiraConta->defineNomeTitular('Ricardo Moreira Soares');
 
 $primeiraConta->deposita(500);
 
@@ -17,3 +21,5 @@ echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
 echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
 
 var_dump($primeiraConta);
+
+echo Conta::recuperaNumeroDeContas();
--- 03-orientacao-objetos-atualizacao/src/Conta.php
+++ 03-orientacao-objetos-atualizacao/src/Conta.php
@@ -4,7 +4,23 @@ class Conta
 {
     public $cpfTitutar;
     public $nomeTitular;
-    private $saldo = 0;
+    private $saldo;
+    private static $numeroDeContas = 0;
+
+    public function __construct(string $cpfTitular, string $nomeTitular)
+    {
+        $this->cpfTitutar = $cpfTitular;
+        $this->validaNomeTitular($nomeTitular);
+        $this->nomeTitular = $nomeTitular;
+        $this->saldo = 0;
+
+        self::$numeroDeContas++;
+    }
+
+    public function __destruct()
+    {
+        self::$numeroDeContas--;
+    }
 
     public function saca(float $valorASacar): void
     {
@@ -40,23 +56,36 @@ class Conta
         return $this->saldo;
     }
 
-    public function defineCpfTitular(String $cpf)
-    {
-        $this->cpfTitutar = $cpf;
-    }
+    // public function defineCpfTitular(String $cpf)
+    // {
+    //     $this->cpfTitutar = $cpf;
+    // }
 
     public function recuperaCpfTitular()
     {
         return $this->cpfTitutar;
     }
 
-    public function defineNomeTitular(string $nome)
-    {
-        $this->nomeTitular = $nome;
-    }
+    // public function defineNomeTitular(string $nome)
+    // {
+    //     $this->nomeTitular = $nome;
+    // }
 
     public function recuperaNomeTitular()
     {
         return $this->nomeTitular;
+    }
+
+    private function validaNomeTitular(string $nomeTitular)
+    {
+        if (strlen($nomeTitular) < 5) {
+            echo "Nome precisa ter pelo menos 5 caracteres";
+            exit();
+        }
+    }
+
+    public static function recuperaNumeroDeContas()
+    {
+        return self::$numeroDeContas;
     }
 }
