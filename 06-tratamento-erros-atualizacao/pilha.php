50ad62e129bb9a81792d341e359e81d0f5a4edd4
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2020-11-20 20:03:53 -0300

    06 atualizacao - tratamento de errors e exceptions

--- 06-tratamento-erros-atualizacao/pilha.php
+++ 06-tratamento-erros-atualizacao/pilha.php
@@ -3,13 +3,24 @@
 function funcao1()
 {
     echo 'Entrei na função 1' . PHP_EOL;
-    funcao2();
+    try {
+        funcao2();
+    } catch (RuntimeException | DivisionByZeroError $problema) {
+        echo $problema->getMessage() . PHP_EOL;
+        echo $problema->getLine() . PHP_EOL;
+        echo $problema->getTraceAsString() . PHP_EOL;
+    }
     echo 'Saindo da função 1' . PHP_EOL;
 }
 
 function funcao2()
 {
     echo 'Entrei na função 2' . PHP_EOL;
+
+    $arrayFixo = new SplFixedArray(2);
+    $arrayFixo[3] = 'Valor';
+    $divisao = intdiv(5, 0);
+
     for ($i = 1; $i <= 5; $i++) {
         echo $i . PHP_EOL;
     }
