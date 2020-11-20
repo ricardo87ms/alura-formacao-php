f652265a1d119dcce9fb70b7b212b8b6540d70a2
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2020-11-20 20:27:56 -0300

    06 - Lançando exceções

--- 06-tratamento-erros-atualizacao/pilha.php
+++ 06-tratamento-erros-atualizacao/pilha.php
@@ -9,6 +9,11 @@ function funcao1()
         echo $problema->getMessage() . PHP_EOL;
         echo $problema->getLine() . PHP_EOL;
         echo $problema->getTraceAsString() . PHP_EOL;
+        throw new RuntimeException(
+            'Exceção foi tratada, mas pega aí',
+            1,
+            $problema
+        );
     }
     echo 'Saindo da função 1' . PHP_EOL;
 }
@@ -17,13 +22,16 @@ function funcao2()
 {
     echo 'Entrei na função 2' . PHP_EOL;
 
-    $arrayFixo = new SplFixedArray(2);
-    $arrayFixo[3] = 'Valor';
-    $divisao = intdiv(5, 0);
+    $exception = new RuntimeException();
+    throw $exception;
 
-    for ($i = 1; $i <= 5; $i++) {
-        echo $i . PHP_EOL;
-    }
+    // $arrayFixo = new SplFixedArray(2);
+    // $arrayFixo[3] = 'Valor';
+    // $divisao = intdiv(5, 0);
+
+    // for ($i = 1; $i <= 5; $i++) {
+    //     echo $i . PHP_EOL;
+    // }
     echo 'Saindo da função 2' . PHP_EOL;
 }
 
